202ab0069c362e089ec910e57a41770249562378
David Hofmann <hofmanndavid@gmail.com>
2021-11-03 11:11:06 -0300

    .

--- src/main/java/io/hdavid/jumper/Portal.java
+++ src/main/java/io/hdavid/jumper/Portal.java
@@ -1,9 +1,12 @@
 package io.hdavid.jumper;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.net.Socket;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+@Slf4j
 public class Portal {
 
     static final ExecutorService es = Executors.newCachedThreadPool();
@@ -13,15 +16,18 @@ public class Portal {
         Integer port = Integer.parseInt(args[1]);
         while (true) {
             Socket socket = new Socket(host, port);
-            L.log("Portal handler socket client oppened");
+            log.info("Portal handler socket client opened");
 //            socket.setTcpNoDelay(true);
             socket.setSoTimeout(60000);
             int firstByte = -1;
             try {firstByte = socket.getInputStream().read();} catch (Exception IGNORE){}
-            L.log("Portal First byte read");
-            if (firstByte < 0)
-                break;
-            es.execute(new PortalAcceptor(socket, firstByte));
+            log.info("Portal First byte read");
+            if (firstByte >= 0) {
+                es.execute(new PortalAcceptor(socket, firstByte));
+            } else {
+                socket.close();
+//                break;
+            }
         }
     }
 }
--- src/main/java/io/hdavid/jumper/PortalHandler.java
+++ src/main/java/io/hdavid/jumper/PortalHandler.java
@@ -1,12 +1,14 @@
 package io.hdavid.jumper;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 import java.util.concurrent.TimeUnit;
 
 @AllArgsConstructor
+@Slf4j
 public class PortalHandler implements Runnable {
     private final Socket portal;
 
@@ -16,7 +18,7 @@ public class PortalHandler implements Runnable {
         try {
             js = Jumper.portalConnections.poll(57, TimeUnit.SECONDS);
             if (js == null) {
-                L.log("Portal Handler discarding poratl connection due to no Jumpers available");
+                log.info("Portal Handler discarding poratl connection due to no Jumpers available");
                 try { portal.close(); } catch (Exception IGNORED) { }
                 return;
             } else {
@@ -24,11 +26,11 @@ public class PortalHandler implements Runnable {
                 portal.getOutputStream().write(js.remoteHostAndPort.getBytes(StandardCharsets.US_ASCII));
                 portal.getOutputStream().write('\n');
                 portal.getOutputStream().flush();
-                L.log("Portal Handler just wrote bridge address");
+                log.info("Portal Handler just wrote bridge address");
                 Jumper.es.execute(new DualIoStream(js.orig, portal));
-                L.log("Portal Handler DualIOStream orig to portal");
+                log.info("Portal Handler DualIOStream orig to portal");
                 Jumper.es.execute(new DualIoStream(portal, js.orig));
-                L.log("Portal Handler DualIOStream portal to orig");
+                log.info("Portal Handler DualIOStream portal to orig");
             }
         } catch (Exception e){
             e.printStackTrace();
